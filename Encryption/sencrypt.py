27d6eb7d176da6ffa11c42d0faa9a30636f64368
Enoch Sem <enochsem@gmail.com>
2023-01-26 22:54:51 +0000

    added vigenereDecipher

--- Encryption/sencrypt.py
+++ Encryption/sencrypt.py
@@ -40,18 +40,36 @@ class SEncrypt():
     
     def vigenereCipher(self, message, key):
         finalData = []
-        message = removeSpace(message) #remove whitespace
-        newKey = keyFill(key, len(message), len(key)) #key fill
+        message = self.removeSpace(message) #remove whitespace
+        newKey = self.keyFill(key, len(message), len(key)) #key fill
         for letter in range(len(message)):
-            cipherIndex = alphabets.index(message[letter]) + alphabets.index(newKey[letter])
+            cipherIndex = ALPHABETS.index(message[letter]) + ALPHABETS.index(newKey[letter])
             cipherIndexOverflow = abs(alphabetsLength - cipherIndex)
             if cipherIndex < alphabetsLength:
-                finalData.append(alphabets[cipherIndex])
+                finalData.append(ALPHABETS[cipherIndex])
             else:
-                finalData.append(alphabets[cipherIndexOverflow])
+                finalData.append(ALPHABETS[cipherIndexOverflow])
         finalData = addSpace(finalData)
         return "".join(finalData)
     
+
+    def vigenereDecipher(cipherText, key):
+        global alphabetsLength
+        finalData = []
+        cipherText = removeSpace(cipherText)  #remove whitespace
+        newKey = keyFill(key, len(cipherText), len(key)) #key fill
+        for c in range(len(cipherText)):
+            msgIndex = alphabets.index(cipherText[c]) - alphabets.index(newKey[c])
+            msgIndexOverflow = abs(msgIndex - (alphabetsLength-1))
+            if msgIndex < 0:
+                msgIndex = msgIndex + alphabetsLength # +26 if msgIndex is negative
+                finalData.append(alphabets[msgIndex])
+            elif msgIndex <= (alphabetsLength-1) and msgIndex >= 0: #0 <= msgIndex <= 25
+                finalData.append(alphabets[msgIndex])
+            else: # pick from index value
+                finalData.append(alphabets[abs(msgIndexOverflow)-1])
+        finalData = addSpace(finalData)
+        return "".join(finalData)
 
 
 if __name__ == "__main__":
